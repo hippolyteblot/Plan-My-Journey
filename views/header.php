68e7278485f7b558b00f1c6d2b0dc1b4cd936c21
mateo <mateo.guenot20@gmail.com>
2022-10-31 12:10:24 +0100

    V1 Header

--- views/header.php
+++ views/header.php
@@ -6,21 +6,28 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>DOCUMENT</title>
-  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../assets/css/header.css" />
+  <script src="https://kit.fontawesome.com/f06f56c2b1.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
   <nav>
     <ul>
       <div class="left">
-        <li><a href="index.html">LOGO</a></li>
+        <li><a href="index.html"><img src="../assets/img/logo.png" alt="logo"></a></li>
         <li><span>Plan My Journey</span></li>
       </div>
+      <input type="checkbox" id="check" />
+      <label for="check">
+        <i class="fas fa-chevron-circle-down"></i>
+      </label>
       <div class="right">
         <li>
-          <a href="#"><span>Découvrir</span></a>
+          <a href="#"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M352 224c53 0 96-43 96-96s-43-96-96-96s-96 43-96 96c0 4 .2 8 .7 11.9l-94.1 47C145.4 170.2 121.9 160 96 160c-53 0-96 43-96 96s43 96 96 96c25.9 0 49.4-10.2 66.6-26.9l94.1 47c-.5 3.9-.7 7.8-.7 11.9c0 53 43 96 96 96s96-43 96-96s-43-96-96-96c-25.9 0-49.4 10.2-66.6 26.9l-94.1-47c.5-3.9 .7-7.8 .7-11.9s-.2-8-.7-11.9l94.1-47C302.6 213.8 326.1 224 352 224z" />
+            </svg><span>Découvrir</span></a>
         </li>
-        <li><a href="#">Connectez-vous</a></li>
+        <li><a href="#"><span>Connectez-vous</span></a></li>
       </div>
     </ul>
   </nav>
