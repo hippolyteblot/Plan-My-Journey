717b7c4dcba2219bbe67f1f706687d9be589daff
mateo <mateo.guenot20@gmail.com>
2022-10-31 10:50:44 +0100

    Ajout de tous les fichiers principaux ainsi que la documentation

---
+++ views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>DOCUMENT</title>
+  <link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <div class="left">
+        <li><a href="index.html">LOGO</a></li>
+        <li><span>Plan My Journey</span></li>
+      </div>
+      <div class="right">
+        <li>
+          <a href="#"><span>Découvrir</span></a>
+        </li>
+        <li><a href="#">Connectez-vous</a></li>
+      </div>
+    </ul>
+  </nav>
+</body>
+
+</html>
