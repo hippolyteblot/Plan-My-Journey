06405710fb501f37dd7b589c212abad39aa485aa
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-01 18:43:37 +0100

    search bar prototype

--- views/accueil.php
+++ views/accueil.php
@@ -5,6 +5,12 @@
   <div>
     <h1>Découvrer votre planning de rêve</h1>
     <h2>Utilisez notre moteur de recherche et trouvez votre journée de rêve en un clic</h2>
+    <form action="index.php?page=query" method="post">
+      <div class="formItem item01"><input id="input01" type="text" name="search" placeholder="Rechercher un événement"></div>
+      <div class="formItem item02">
+        <input id="input02" type="image" src="<?= PATH_IMAGES ?>search.png" alt="Submit" style="float:right" width="40" height="40">
+
+      </div>
   </div>
   <span>
     Swip up !
