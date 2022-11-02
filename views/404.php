fd2e1da1e01e35bb896b1c07e32dc289be13b6c8
p2109548 <noah.heraud@etu.univ-lyon1.fr>
2022-11-02 16:25:55 +0100

    page 404 avec button de retour

--- views/404.php
+++ views/404.php
@@ -5,13 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= PATH_CSS ?>reset.css">
     <link rel="stylesheet" href="<?= PATH_CSS ?>404.css">
+
     <title>Document</title>
 </head>
 
 <body>
     <main>
         <h1 id="titleErr"> erreur 404 </h1>
+        <div class="container">
+
+            <a href="index.php" class="button">
+                <div class="leftArrow"></div>
+                <p>Accueil</p>
+            </a>
+        </div>
     </main>
 </body>
 
