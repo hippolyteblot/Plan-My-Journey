8752d97f3d7d890adeba728563438eb8b3af5160
Hippo <hippolytebl@gmail.com>
2022-11-12 18:12:18 +0100

    V1 du front-end de la sélection des paramètres du parcours

--- controllers/parametersSelection.php
+++ controllers/parametersSelection.php
@@ -5,10 +5,5 @@ $pageName = "Sélection des paramètres";
 if(!isset($_SESSION['candidates'])) {
     header('Location: index.php?page=home');
 }
-$name = $_SESSION['candidates'][0]['name'];
-$lat = $_SESSION['candidates'][0]['geometry']['location']['lat'];
-$lng = $_SESSION['candidates'][0]['geometry']['location']['lng'];
-$fullAddress = $_SESSION['candidates'][0]['formatted_address'];
-
 
 require_once(PATH_VIEWS . 'parametersSelection.php');
--- views/parametersSelection.php
+++ views/parametersSelection.php
@@ -13,33 +13,62 @@
     
     <form action="" method="post">
         <div class="formItem">
-            <h2>Lieux selectionné : <?= $fullAddress ?></h2>
+            <h2>Lieux selectionné : </h2>
+            <select name="lieux" id="lieux">
+                <?php
+                foreach ($_SESSION['candidates'] as $place) {
+                    echo "<option value='".$place['formatted_address']."'>".$place['formatted_address']."</option>";
+                }
+                ?>
+            </select>
         </div>
-        <div class="formItem time">
-            <h2 for="time">Quelles horaires ?</h2>
-            <!-- From ? to ? -->
-            <div class="time inputGroup">
-                <div class="timeItem">
-                    <label for="timeFrom">De </label>
-                    <input type="time" name="timeFrom" id="timeFrom" value="00:00">
-                </div>
-                <div class="timeItem">
-                    <label for="timeTo"> à </label>
-                    <input type="time" name="timeTo" id="timeTo" value="23:59">
+        <div id="options">
+            <div class="formItem">
+                <h2 for="time">Quelles horaires ?</h2>
+                <!-- From ? to ? -->
+                <div class="time inputGroup">
+                    <div class="timeItem">
+                        <label for="timeFrom">De </label>
+                        <input type="time" name="timeFrom" id="timeFrom" value="00:00">
+                    </div>
+                    
+                    <div class="timeItem">
+                        <label for="timeTo">A </label>
+                        <input type="time" name="timeTo" id="timeTo" value="23:59">
+                    </div>
                 </div>
             </div>
-            <h2 for="budget">Quel budget ?</h2>
-            <div class="budget inputGroup">
-                <div class="budgetItem">
-                    <!-- Choice between 3 budgets -->
-                    <label for="budget">Budget : </label>
-                    <select name="budget" id="budget">
-                        <option value="1">€</option>
-                        <option value="2">€€</option>
-                        <option value="3">€€€</option>
+            <div class="formItem">
+                <h2 for="budget">Quel budget ?</h2>
+                <div class="budget inputGroup">
+                    <div class="budgetItem">
+                        <!-- Choice between 3 budgets -->
+                        <label for="budget">Budget : </label>
+                        <select name="budget" id="budget">
+                            <option value="1">€</option>
+                            <option value="2">€€</option>
+                            <option value="3">€€€</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="formItem">
+                <h2 for="type">Prévoir à manger ?</h2>
+                <div class="type inputGroup">
+                    <label for="restaurant">Restauration : </label>
+                    <select name="restaurant" id="restaurant">
+                        <option value="1">Oui</option>
+                        <option value="0">Non</option>
                     </select>
                 </div>
             </div>
+            <div class="formItem">
+                <h2 for="type">Continuer ?</h2>
+                <div class="type inputGroup">
+                    <button type="submit" name="submit" id="submit">Valider</button>
+                </div>
+            </div>
+            
         </div>
       
     </form>
