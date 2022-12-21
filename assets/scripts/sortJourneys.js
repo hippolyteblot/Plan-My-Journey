56819784f05bbbe61749f7d05c20b6e06f17ac53
Hippo <hippolytebl@gmail.com>
2022-12-21 16:34:16 +0100

    Fin de l'implémentation du système de tri

--- assets/scripts/sortJourneys.js
+++ assets/scripts/sortJourneys.js
@@ -2,22 +2,40 @@ var journeys = document.querySelectorAll('.journey-preview');
 
 var journeysArray = Array.prototype.slice.call(journeys);
 
-function sortJourneysByDate() {
-    journeysArray.sort(function(a, b) {
-        return new Date(b.dataset.date) - new Date(a.dataset.date);
-    });
+function sortJourneysByDate(asc) {
+    if (asc) {
+        journeysArray.sort(function(a, b) {
+            return new Date(a.dataset.date) - new Date(b.dataset.date);
+        });
+    } else {
+        journeysArray.sort(function(a, b) {
+            return new Date(b.dataset.date) - new Date(a.dataset.date);
+        });
+    }
 }
 
-function sortJourneysByRating() {
-    journeysArray.sort(function(a, b) {
-        return b.dataset.rating - a.dataset.rating;
-    });
+function sortJourneysByRating(asc) {
+    if (asc) {
+        journeysArray.sort(function(a, b) {
+            return a.dataset.rating - b.dataset.rating;
+        });
+    } else {
+        journeysArray.sort(function(a, b) {
+            return b.dataset.rating - a.dataset.rating;
+        });
+    }
 }
 
-function sortJourneysByDistance() {
-    journeysArray.sort(function(a, b) {
-        return b.dataset.distance - a.dataset.distance;
-    });
+function sortJourneysByDistance(asc) {
+    if (asc) {
+        journeysArray.sort(function(a, b) {
+            return a.dataset.distance - b.dataset.distance;
+        });
+    } else {
+        journeysArray.sort(function(a, b) {
+            return b.dataset.distance - a.dataset.distance;
+        });
+    }
 }
 
 function deleteJourneysFromDOM() {
@@ -28,6 +46,58 @@ function deleteJourneysFromDOM() {
 
 function addJourneysToDOM() {
     journeysArray.forEach(function(journey) {
-        document.querySelector('.journey-container').appendChild(journey);
+        document.querySelector('.journey-container-grid').appendChild(journey);
     });
+}
+
+function manageDateSort(element) {
+    var selected = element.classList.contains('selected');
+    var sortButtons = document.querySelectorAll('.sort-button');
+    sortButtons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+    element.classList.add('selected');
+    if(!selected) {
+        if(element.innerHTML === 'Date ↓') {
+            sortJourneysByDate(true);
+        } else {
+            sortJourneysByDate(false);
+        }
+    } else {
+        if(element.innerHTML === 'Date ↓') {
+            element.innerHTML = 'Date ↑';
+            sortJourneysByDate(false);
+        } else {
+            element.innerHTML = 'Date ↓';
+            sortJourneysByDate(true);
+        }
+    }
+    deleteJourneysFromDOM();
+    addJourneysToDOM();
+}
+
+function manageRatingSort(element) {
+    var selected = element.classList.contains('selected');
+    var sortButtons = document.querySelectorAll('.sort-button');
+    sortButtons.forEach(function(button) {
+        button.classList.remove('selected');
+    });
+    element.classList.add('selected');
+    if(!selected) {
+        if(element.innerHTML === 'Note ↓') {
+            sortJourneysByRating(true);
+        } else {
+            sortJourneysByRating(false);
+        }
+    } else {
+        if(element.innerHTML === 'Note ↓') {
+            element.innerHTML = 'Note ↑';
+            sortJourneysByRating(false);
+        } else {
+            element.innerHTML = 'Note ↓';
+            sortJourneysByRating(true);
+        }
+    }
+    deleteJourneysFromDOM();
+    addJourneysToDOM();
 }
--- views/discover.php
+++ views/discover.php
@@ -28,12 +28,12 @@
               </button>
           </form>
         </div>
-      <!--<button class="filter-button glass" onclick="openModal('trier')">Trier</button>-->
-      <button class="filter-button glass" onclick="deleteJourneysFromDOM(); sortJourneysByDate(); addJourneysToDOM();">Date</button> <!-- Changer pour laisser le choix pour le tri -->
+      <button class="filter-button glass" onclick="openModal('trier')">Trier</button>
+      <!--<button class="filter-button glass" onclick="deleteJourneysFromDOM(); sortJourneysByDate(); addJourneysToDOM();">Date</button> Changer pour laisser le choix pour le tri -->
       
     </div>
     <h2>Voici un ensemble de parcours partagés par la communauté</h2>
-    <div class="journey-container">
+    <div class="journey-container-grid">
         <?php foreach ($journeysArray as $journey) {
             include(PATH_VIEWS . 'journeyPreview.php');
         } ?>
@@ -46,54 +46,13 @@
   <!-- Modal content -->
   <div class="modal-content">
     <div class="modal-header">
-      <span class="close" onclick="closeModal('filter')">&times;</span>
-        <h2>Filtrer</h2>
+      <span class="close" onclick="closeModal('trier')">&times;</span>
+        <h2>Filtrer les parcours par :</h2>
     </div>
     <!-- Allow to filter from rating, location, corresponding preferences and date -->
     <div class="modal-body">
-      <form action="index.php?page=discover" method="post">
-        <div class="form-group">
-          <label for="rating">Note minimum</label>
-          <select name="rating" id="rating">
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="location">Localisation</label>
-          <input type="text" name="location" id="location" placeholder="Localisation">
-        </div>
-        <div class="form-group">
-          <label for="date">Horaires</label>
-                <div class="time inputGroup">
-                    <div class="timeItem">
-                        <label for="timeFrom">De </label>
-                        <input type="time" name="timeFrom" id="timeFrom" value="10:00">
-                    </div>
-                    
-                    <div class="timeItem">
-                        <label for="timeTo">A </label>
-                        <input type="time" name="timeTo" id="timeTo" value="18:00">
-                    </div>
-                </div>
-        </div>
-
-        <div class="form-group">
-          <label for="preferences">Préférences</label>
-          <select name="preferences" id="preferences">
-            <option value="0">Toutes</option>
-            <option value="1">Principales</option>
-            <option value="2">Secondaires</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <input type="submit" value="Filtrer">
-        </div>
-      </form>
+      <button class="sort-button glass" onclick="manageDateSort(this);">Date ↓</button>
+      <button class="sort-button glass" onclick="manageRatingSort(this);">Note ↓</button>
     </div>
-    </div>
+  </div>
 </div>
