dbef172053cb17d75f9d30d9c23741fd63296353
WinDyAlphA <heraud260@gmail.com>
2022-12-30 22:54:00 +0100

    404 modified

--- views/404.php
+++ views/404.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?= PATH_CSS ?>reset.css">
+    <link rel="stylesheet" href="<?= PATH_CSS ?>glassmorphism.css" />
     <link rel="stylesheet" href="<?= PATH_CSS ?>404.css">
+    
 
     <title>Document</title>
 </head>
 
 <body>
-    <main>
+    <main class="glass">
         <h1 id="titleErr"> erreur 404 </h1>
         <div class="container">
 
