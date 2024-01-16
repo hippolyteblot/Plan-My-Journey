a3d1d90a07e2fc1bb74bdbaa48817d520dc92f58
Hippolyte Blot <72436409+hippolyteblot@users.noreply.github.com>
2024-01-16 14:13:29 +0100

    Update

--- config/config.php
+++ config/config.php
@@ -16,8 +16,7 @@ const AUTEUR = 'Matéo Guenot & Noah Heraud & Hippolyte Blot';
 const MAIL = 'planmyjourney@gmail.com';
 
 // Clé privé de l'API Google
-//const KEY = "AIzaSyDsSWpa4y-dsYA7BBA-I4xJq60be0qAHUI"; // <- Hippo
-const KEY = "AIzaSyBAbAVbxk-8sF-HlDk92YeQ9wop5tH6yyc"; // <- Noah
+const KEY = "";
 
 
 const ADMIN_NAME = 'admin';
